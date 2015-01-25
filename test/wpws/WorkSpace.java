b94760eececb112be61a20e4dee0dc311db86d85
ebabalac <ebabalac@gmail.com>
2015-01-25 14:24:04 +0100

    add junit test tag

--- test/wpws/WorkSpace.java
+++ test/wpws/WorkSpace.java
@@ -1,15 +1,21 @@
 package wpws;
 
+import static org.junit.Assert.fail;
+
 import java.util.List;
+
+import org.junit.Test;
 
 public class WorkSpace {
 	
+	@Test
 	public void testWSStatus(){
-		
+		fail("Not yet implemented");
 	}
 	
+	@Test
 	public void testAddedWPtoWS(){
-		
+		fail("Not yet implemented");
 	}
 	
 }
