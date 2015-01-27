00e4ca0a0f18b21952f3b70bc0a5ed276f8b8b02
BA27 <fatoumataba27@gmail.com>
2015-01-27 17:28:27 +0100

    method setter and getter

--- src/utilisateur/UserFactory.java
+++ src/utilisateur/UserFactory.java
@@ -2,10 +2,62 @@ package utilisateur;
 
 
 public class UserFactory {
+	private String ID;
+	private String prenom;
+	private String nom;
+	//private String adresse;
+	
+	public UserFactory (String ID, String prenom, String nom)
+	{
+		this.ID = ID;
+		this.prenom = prenom;
+		this.nom = nom;
+	}
+	
+	public String getID()
+	{
+		return ID;
+	}
+	
+	public void setId( String ID)
+	{
+		this.ID = ID;
+	}
+	
+	public String getNom ()
+	{
+		return nom;
+	}
+	public void SetNom (String nom)
+	{
+		this.nom = nom;
+	}
+	public String getPrenom()
+	{
+		return prenom ;
+	}
+	
+	public void setPrenom (String prenom)
+	{
+		this.prenom = prenom;
+	}
+		
 	//Permet de créer un utilisateur et de lui assigner des droits grâce à une Ability.
-	public void createUser(String  nom,String prenom, String ID){}
+	public void createUser(String  nom,String prenom, String ID){
+		UserFactory u = new UserFactory(ID, prenom, prenom);
+		
+		
+	}
 	//Permet de supprimer  un utilisateur mais le donner de son compte doit toujours exister
-	public void deleteUser(String ID){}
+	public void deleteUser(String ID){
+		
+	}
+	
 	//Permet de supprimer  un utilisateur mais le donner de son compte doit toujours exister
-	public void addUser(String  nom, String prenom, String ID){}
+	public void addUser(String  nom, String prenom, String ID){
+		
+	}
+	
+	
+	
 }
