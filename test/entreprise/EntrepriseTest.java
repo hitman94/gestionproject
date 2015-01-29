5171f978d163b90ee4a21b1a338f7af3d48ac428
Thomas Londechamp <totolond@hotmail.com>
2015-01-29 02:51:55 +0100

    mise en place des classes de test pour le composant Entreprise

---
+++ test/entreprise/EntrepriseTest.java
@@ -0,0 +1,14 @@
+package entreprise;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class EntrepriseTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ test/entreprise/GroupTest.java
@@ -0,0 +1,14 @@
+package entreprise;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GroupTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
