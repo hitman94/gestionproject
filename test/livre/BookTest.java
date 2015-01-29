4b3adb751ca65ee9c86be0734757a7e709d12725
Thomas Londechamp <totolond@hotmail.com>
2015-01-29 02:22:07 +0100

    4 Tests pour la classe Book

--- test/livre/BookTest.java
+++ test/livre/BookTest.java
@@ -7,12 +7,26 @@ import org.junit.Test;
 public class BookTest {
 
 	@Test
+	public void bookConstructorTest(){
+		Book b = new Book(new Long("1"), "Book 1", "Book Author");
+		assertEquals(new Long("1"), b.getId());
+		assertEquals("Book 1", b.getTitle());
+		assertEquals("Book Author", b.getAuthor());
+	}
+	
+	@Test(expected = NullPointerException.class)
+	public void addNullVolumeTest() {
+		Book b = new Book(new Long("1"), "Book 1", "Book Author");
+		Volume v = null;
+		b.addVolume(v);
+	}
+	
+	@Test
 	public void addVolume2TimesTest() {
 		Book b = new Book(new Long("1"), "Book 1", "Book Author");
 		Volume v = new Volume(new Long("10"), "Volume 1", "Volume Author");
 		b.addVolume(v);
-		b.addVolume(v);
-		assertEquals(v.getId() + " ne doit pas être ajouté 2 fois.", b.getVolumes().size(), 1);
+		assertNotEquals(v.getTitle() + " ne peut être ajouté qu'1 fois.", true, b.addVolume(v));
 	}
 	
 	public void removeVolume2TimesTest() {
@@ -20,7 +34,7 @@ public class BookTest {
 		Volume v = new Volume(new Long("10"), "Volume 1", "Volume Author");
 		b.addVolume(v);
 		b.removeVolume(v);
-		assertNotEquals(v.getId() + " ne peut être supprimé qu'1 fois.", b.removeVolume(v), true);
+		assertNotEquals(v.getTitle() + " ne peut être supprimé qu'1 fois.", true, b.removeVolume(v));
 	}
 
 }
