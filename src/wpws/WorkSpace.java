a928542ac50a56f29f92627c0a65b291a7f2d1b2
ebabalac <ebabalac@gmail.com>
2015-01-31 22:37:40 +0100

    update WorkSpace and ad some test

--- src/wpws/WorkSpace.java
+++ src/wpws/WorkSpace.java
@@ -1,23 +1,71 @@
 package wpws;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.UUID;
 
+/**
+ * Le workspace est composée d'un identifiant unique et d'une collection de workpackage qui le compose
+ * @author emmanuel
+ *
+ */
 public class WorkSpace {
-	List<WorkPackage> wpList;
+	private final UUID id;
+	private final Set<WorkPackage> wpList;	
 
-	/**
-	 * recupere la liste des WP du Workspace
-	 * @return
-	 */
-	public List<WorkPackage> getWpList() {
-		return wpList;
+	public WorkSpace() {
+		id = UUID.randomUUID();
+		wpList = new HashSet<WorkPackage>();
 	}
 
 	/**
-	 * recupère la maturite du WS
+	 * recupere la liste des WP du Workspace
+	 * 
 	 * @return
 	 */
+	public List<WorkPackage> getWpList() {
+		return new ArrayList<WorkPackage>(wpList);
+	}
+
+	public void addWP(WorkPackage wp) {
+		wpList.add(wp);
+	}
+
+	public boolean removeWP(WorkPackage wp) {
+		return wpList.remove(wp);
+	}
+	
+	/**
+	 * recupere l'identifiant unique du workspace
+	 * @return
+	 */
+	public UUID getId() {
+		return id;
+	}
+	/**
+	 * recupère la maturite du WS
+	 * 
+	 * @return
+	 */
+
 	public WSMaturity.State getWSMaturity() {
-		throw new UnsupportedOperationException();
+
+		// algorithme basic
+		// la maturité du ws depend de celle des wp$
+		// si un wp est a un etat inferieur alors le ws est a cette etape
+		// lews est done lorsque tous les wp sont done
+		for (WorkPackage wp : wpList) {
+			wpws.WPMaturity.State wpMaturity = wp.getWPMaturity();
+			if (wpMaturity.equals(WPMaturity.State.Start)) {
+				return WSMaturity.State.Start;
+
+			} else if (wpMaturity.equals(WPMaturity.State.InProgress)) {
+				return WSMaturity.State.InProgress;
+			}
+
+		}
+		return WSMaturity.State.Done;
 	}
 }
--- test/wpws/WorkSpaceTest.java
+++ test/wpws/WorkSpaceTest.java
@@ -2,43 +2,103 @@ package wpws;
 
 import static org.junit.Assert.fail;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class WorkSpaceTest {
-	
-	/**
-	 * test if we get the right status
-	 */
-	@Test
-	public void testWSStatus(){
-		fail("Not yet implemented");
-	}
+
 	
 	/**
 	 * test if we can add WS to WP
 	 */
 	@Test
-	public void testAddedWPtoWS(){
-		fail("Not yet implemented");
+	public void testAddedWPtoWS() {
+		WorkPackage wp = new WorkPackage();
+		WorkSpace ws = new WorkSpace();
+		ws.addWP(wp);
+		Assert.assertTrue(ws.getWpList().get(0).equals(wp));
 	}
-	
+
 	/**
 	 * test if we can remove WP to a WS
 	 */
 	@Test
-	public void testRemovedWPtoWS(){
-		fail("Not yet implemented");
+	public void testRemovedWPtoWS() {
+		WorkPackage wp = new WorkPackage();
+		WorkSpace ws = new WorkSpace();
+		ws.addWP(wp);
+		Assert.assertTrue(ws.removeWP(wp));
+		Assert.assertTrue(ws.getWpList().size()==0);
 	}
+
+	/**
+	 * test if we get the right status
+	 */
+	@Test
+	public void testWSStatus() {
+		WorkSpace ws = new WorkSpace();
+		WorkPackage wp1 = new WorkPackage();
+		wp1.setStatus(WSMaturity.State.Start);
+		WorkPackage wp2 = new WorkPackage();
+		wp2.setStatus(WSMaturity.State.Start);
+		WorkPackage wp3 = new WorkPackage();
+		wp3.setStatus(WSMaturity.State.Start);		
+		ws.addWP(wp1);
+		ws.addWP(wp2);
+		ws.addWP(wp3);
+		
+		Assert.assertTrue(ws.getWSMaturity().equals(WSMaturity.State.Start));
+		wp1.setStatus(WSMaturity.State.Start);
+		wp2.setStatus(WSMaturity.State.InProgress);
+		wp3.setStatus(WSMaturity.State.InProgress);
+		Assert.assertTrue(ws.getWSMaturity().equals(WSMaturity.State.Start));
+		wp1.setStatus(WSMaturity.State.InProgress);
+		wp2.setStatus(WSMaturity.State.InProgress);
+		wp3.setStatus(WSMaturity.State.InProgress);		
+		Assert.assertTrue(ws.getWSMaturity().equals(WSMaturity.State.InProgress));
+		wp1.setStatus(WSMaturity.State.InProgress);
+		wp2.setStatus(WSMaturity.State.Done);
+		wp3.setStatus(WSMaturity.State.InProgress);		
+		Assert.assertTrue(ws.getWSMaturity().equals(WSMaturity.State.InProgress));
+		wp1.setStatus(WSMaturity.State.Done);
+		wp2.setStatus(WSMaturity.State.Done);
+		wp3.setStatus(WSMaturity.State.Done);		
+		Assert.assertTrue(ws.getWSMaturity().equals(WSMaturity.State.Done));
+		
+		
+	}
+
 	
 	/**
-	 * test if status of WS change
-	 * if condition of change id state :
-	 * e.g do we get status inprogress after a promote on a WP for instance
+	 * test if status of WS change if condition of change id state : e.g do we
+	 * get status inprogress after a promote on a WP for instance
 	 * 
 	 */
 	@Test
-	public void testIfWSStatusChange(){
-		fail("Not yet implemented");
+	public void testIfWSStatusChange() {
+		final WorkSpace ws = new WorkSpace();
+		WorkPackage wp1 = new WorkPackage();
+		wp1.setStatus(WSMaturity.State.InProgress);
+		WorkPackage wp2 = new WorkPackage();
+		wp2.setStatus(WSMaturity.State.InProgress);
+			
+		ws.addWP(wp1);
+		ws.addWP(wp2);
+		
+		Assert.assertTrue(ws.getWSMaturity().equals(WSMaturity.State.InProgress));
+		new Thread(new Runnable() {
+			
+			public void run() {
+				WorkPackage wp3 = new WorkPackage();
+				wp3.setStatus(WSMaturity.State.Start);
+				ws.addWP(wp3);
+				
+			}
+		}).start();
+		Assert.assertTrue(ws.getWSMaturity().equals(WSMaturity.State.Start));
+		
 	}
 	
+	
+
 }
