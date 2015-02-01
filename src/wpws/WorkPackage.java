574e28f06b49448060c533cb9d67f949c587a20d
Thomas Guerin <adminmizix@gmail.com>
2015-02-01 17:09:54 +0100

    Debut WP

--- src/wpws/WorkPackage.java
+++ src/wpws/WorkPackage.java
@@ -2,7 +2,12 @@ package wpws;
 
 
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
 
 import livre.Book;
 import livre.Chapter;
@@ -11,14 +16,48 @@ import livre.SubChapter;
 import livre.Volume;
 
 public class WorkPackage {
+	
+	private Set<Book> lBooks = new HashSet<Book>();
+	private Map<Volume,Set<Chapter>> mVol = new HashMap<>();
+	private Map<Chapter,Set<SubChapter>> mChap = new  HashMap<>();
+	
 	//Attribue un livre à un workPackage
-	public void addBook(Book book){}
+	public void addBook(Book book){
+		Objects.requireNonNull(book);
+		lBooks.add(book);
+	}
+	
 	//Attribue un volume à un workPackage
-	public void addVolume(Volume volume){}
+	public void addVolume(Volume volume){
+		Objects.requireNonNull(volume);
+		if(mVol.get(volume)!=null) {
+			mVol.put(volume, new HashSet<Chapter>());
+		}
+	}
+	
 	//Attribue un chapitre à un volume
-	public void addChapterToVolume(Chapter chapterToAdd,Volume volume){}
+	public void addChapterToVolume(Chapter chapterToAdd,Volume volume){
+		
+		Objects.requireNonNull(volume);
+		Objects.requireNonNull(chapterToAdd);
+		Set<Chapter> sChapter = mVol.get(volume);
+		if(sChapter !=null) {
+			if(sChapter.add(chapterToAdd))
+				mChap.put(chapterToAdd, new HashSet<SubChapter>());
+		} else {
+			throw new IllegalArgumentException();
+		}
+	}
 	//Attribue un sous chapitre a un chapitre
-	public void addSubChapterToChapter(SubChapter subToAdd,Chapter chapter){}
+	public void addSubChapterToChapter(SubChapter subToAdd,Chapter chapter){
+		Objects.requireNonNull(subToAdd);
+		Objects.requireNonNull(chapter);
+		Set<SubChapter> sSubChapter = mChap.get(subToAdd);
+		if(sSubChapter!=null)
+			sSubChapter.add(subToAdd);
+		else
+			throw new IllegalArgumentException();
+	}
 	//recupère une liste de chapitre ou de sous chapitre
 	//Si SubChapterNumber different de -1 recupère le sous chapitre appartenant correspondant au chapterNumber
 	//sinon recupere tous le chapitre
@@ -30,7 +69,5 @@ public class WorkPackage {
 	
 	//Ajouter un WorkPackage
 	public WPMaturity.State addWPMaturity(){}
-	
-	//Recupère tous les WP du WS
-	public List<WP> getWPList(){}
+
 }
