2d8b68af42b4c359bea12f4bbf003f23b0f43514
doquanghuyit <doquanghuyit@gmail.com>
2015-02-12 22:19:09 +0700

    SessionUser, this class service us to control information of session's web while user connect to system

---
+++ src/ihm/SessionUser.java
@@ -0,0 +1,63 @@
+/**
+ * 
+ */
+package ihm;
+
+import utilisateur.User;
+
+/**
+ * @author doquanghuy
+ *
+ */
+
+//SessionUser service the session's each user while they connect to system 
+@Named
+@SessionScoped
+public class SessionUser {
+	
+	//After login,if true we get instance's object user from database 
+	//if false user = null
+    private User user;
+    
+    //After connect, we set connect to true, else = false
+    private boolean isConnected=false;
+    
+    //Object Message which service us to pass the type message and content of message 
+    //between pages Servlet and JSP
+    private Message message;
+    
+    public SessionUser() {
+    }
+    
+    public User getUser()
+    {
+        return user;
+    }
+    
+    public void setUser(User user)
+    {
+        this.user=user;
+    }
+    
+    public boolean getIsConnected()
+    {
+        return isConnected;
+    }
+    
+    public void setIsConnected(boolean isConnected)
+    {
+        this.isConnected=isConnected;
+    }
+    
+    public Message getMessage()
+    {
+        return message;
+    }
+    
+    public void setMessage(Message message)
+    {
+        this.message=message;
+    }
+	
+
+}
