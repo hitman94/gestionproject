5b3a8238f6cbd139c32316e12cd8abe4b3dcd364
doquanghuyit <doquanghuyit@gmail.com>
2015-02-12 22:24:22 +0700

    Message CDI

---
+++ src/ihm/Message.java
@@ -0,0 +1,15 @@
+package ihm;
+
+
+//Define Message CDI for type and content of message
+// Message Welcome
+// Message create / modify user
+// Message error
+public class Message {
+	
+	public Message()
+	{
+		
+	}
+
+}
