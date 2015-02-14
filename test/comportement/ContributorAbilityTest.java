efe930b984e0558e3596a98b3a1637c7459faed7
Thomas Londechamp <totolond@hotmail.com>
2015-02-14 17:00:37 +0100

    mise en place tests ability

---
+++ test/comportement/ContributorAbilityTest.java
@@ -0,0 +1,14 @@
+package comportement;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ContributorAbilityTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ test/comportement/PatronAbilityTest.java
@@ -0,0 +1,14 @@
+package comportement;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PatronAbilityTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
