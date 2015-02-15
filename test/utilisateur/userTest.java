93c109b0e4b3b60923cdfc2c1c8f7ba415ea0602
BA27 <fatoumataba27@gmail.com>
2015-02-15 21:51:45 +0100

    quelques test unitaire

--- test/utilisateur/userTest.java
+++ test/utilisateur/userTest.java
@@ -1,11 +1,11 @@
 package utilisateur;
 
 import static org.junit.Assert.*;
-
 import org.junit.Test;
 
 import comportement.Ability;
 import comportement.CompanyChiefAbility;
+import entreprise.Entreprise;
 
 public class userTest {
 
@@ -25,42 +25,58 @@ public class userTest {
 		if(u.getAbility()==null)
 			throw new IllegalArgumentException();
 	}
-
 	@Test 
 	public void testNullUserName(){
-
+		CompanyChiefAbility a = null;
+		User u =new  User("","12344",a) ;
+		if(u.getUserName()==null)
+			throw new IllegalArgumentException();
 	}
 	@Test
 	public void testNullPassWord(){
+		// test pour verifier que le mot de passe n'est pas null
+		CompanyChiefAbility a = null;
+		User u =new  User("Fatou","",a) ;
+		if(u.getPassWord()== null)
+			throw new IllegalArgumentException();
 
 	}
+	
+	@Test 
+	/*public void createUserTest(){
+		// Test pour verifier que les utilisateurs n'ont pas les mêmes nom
+		//pour la création d'un utilisateur
+		CompanyChiefAbility a = null;
+		User u =new  User("Fatou","12344",a);
+		u.
+		if(u.equals(u))
+			throw new IllegalArgumentException();	
+	}*/
 	@Test 
 	public void testUserBelongToEntreprise(){
 		//test si le user appartient Ã  l'entreprise
+		Entreprise e = null;
+		CompanyChiefAbility a = new CompanyChiefAbility();
+		User u =new  User("Fatou","12344", a) ;
+		e.addMember(u);
+		assertEquals(u, e.getMembers());
 	}
 	@Test
 	public void testUserBelongToGroupe(){
-		//entreprise.getmember()
-
+		//test pour verifier s'il fait partie de la liste des utilisateurs
+		Entreprise e = null;
+		CompanyChiefAbility a = new CompanyChiefAbility();
+		User u =new  User("Fatou","12344", a);	
 	}
+	
 	@Test
 	public void testUserIsChef(){
-
+		// test si l'utilisateur est un chef
+		Entreprise e = null;
+		CompanyChiefAbility a = new CompanyChiefAbility();
+		User u =new  User("Fatou","12344", a) ;
+		e.addMember(u);
+		assertEquals(u, e.getChief());	
 	}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	
 }
