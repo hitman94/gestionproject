080c6c189b0ecafe5972bb685dc6bfaa4f7b4629
jmeryam <jmeyam@gmail.com>
2015-02-15 12:51:49 +0100

    Junit user

--- test/utilisateur/userTest.java
+++ test/utilisateur/userTest.java
@@ -1,21 +1,31 @@
 package utilisateur;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 
 import comportement.Ability;
+import comportement.CompanyChiefAbility;
 
 public class userTest {
 
 	@Test
 	public void testUserConstructor() {
-		Ability a ;//How to get user or specify user abilities??
-		User u = new User("Fatou",new Long(12344),null);
-	
+		CompanyChiefAbility a = new CompanyChiefAbility();
+		//Utilisateurs, Chef d'entreprise et Patron du Livre
+		User u =new  User("Fatou","12344",a) ;
 		assertEquals("Fatou", u.getUserName());
-		assertEquals(new Long(12344), u.getPassWord());
-		assertEquals(null, u.getAbility());
+		assertEquals("12344", u.getPassWord());
+		assertEquals("CompanyChiefAbility", a.getClass().getSimpleName());
 	}
+	@Test(expected = IllegalArgumentException.class)
+	public void testNullAbility(){	
+		CompanyChiefAbility a = null;
+		assertFalse(throwException());
+	}
+	private boolean throwException(){
+		throw new IllegalArgumentException();
+	}
+	
 
 }
