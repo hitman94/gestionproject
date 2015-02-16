410ae455168ac160114ffd15336e6b99962792f6
doquanghuyit <doquanghuyit@gmail.com>
2015-02-17 00:01:10 +0700

    Update Message

--- src/ihm/Message.java
+++ src/ihm/Message.java
@@ -7,9 +7,37 @@ package ihm;
 // Message error
 public class Message {
 	
-	public Message()
+	//Type mgs 0 = normal, 1 = error
+	private int type;
+	
+	//Content message
+	private String msg;
+	
+	
+	public Message(int type, String msg)
 	{
-		
+		this.type=type;
+		this.msg=msg;
+	}
+	
+	public int getTypeMsg()
+	{
+		return type;
+	}
+	
+	public void setTypeMsg(int type)
+	{
+		this.type=type;
+	}
+	
+	public String getMessage()
+	{
+		return msg;
 	}
 
+	
+	public void setMessage(String msg)
+	{
+		this.msg=msg;
+	}
 }
