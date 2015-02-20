767629697be9f714418d857f81520b5bf5cd7807
Florian GUIF <flo.guif@gmail.com>
2015-02-20 13:19:38 +0100

    Servlet de connection

---
+++ src/ihm/servletv2/ConnectionServlet.java
@@ -0,0 +1,55 @@
+package ihm.servletv2;
+
+import java.io.IOException;
+
+import javax.inject.Inject;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import utilisateur.User;
+import dao.UserDAO;
+
+/**
+ * Servlet implementation class ConnectionServlet
+ */
+@WebServlet("/ConnectionServlet")
+public class ConnectionServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+      @Inject
+      private UserDAO dao;
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ConnectionServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		User user = dao.connexion(username, password);
+		if(user!=null){
+			response.setStatus(200);
+			HttpSession session = request.getSession();
+			session.setAttribute("user", user);
+		}else{
+			response.sendError(400, "Password ou Username incorrect");
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
