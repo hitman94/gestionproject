eb837f850abf9602c733769c74a5b603dbf7d66b
Florian GUIF <flo.guif@gmail.com>
2015-02-21 11:14:35 +0100

    Mini changement du servlet deconnection

--- src/ihm/servletv2/DeconnectionServlet.java
+++ src/ihm/servletv2/DeconnectionServlet.java
@@ -42,7 +42,7 @@ public class DeconnectionServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		doGet(request, response);
 	}
 
 }
