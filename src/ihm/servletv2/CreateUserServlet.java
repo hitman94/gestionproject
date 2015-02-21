64f6345919fa48a2a60d167f58f48df797cb3f40
ebabalac <ebabalac@gmail.com>
2015-02-21 18:16:49 +0100

    add support of register and unregister user

--- src/ihm/servletv2/CreateUserServlet.java
+++ src/ihm/servletv2/CreateUserServlet.java
@@ -9,12 +9,15 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.codec.digest.DigestUtils;
+
 import comportement.Ability;
 import utilisateur.User;
 import dao.UserDAO;
 
 /**
  * this servlet will manage the register of a new user
+ * 
  * @author emmanuel
  *
  */
@@ -47,18 +50,38 @@ public class CreateUserServlet extends HttpServlet {
 		String username = request.getParameter("username");
 		String password = request.getParameter("password");
 		String role = request.getParameter("role");
-		
+
+		User user = (User) request.getSession().getAttribute("user");
+
 		if (checkValidity(username) && checkValidity(password)
 				&& checkValidity(role)) {
 
 			Ability roleAbility = Ability.valueOf(role);
-			dao.persist(new User(username, password, roleAbility));
+			Ability userAb = user.getAbility();
+			if (userAb.equals(Ability.User)) {
+				response.sendError(HttpServletResponse.SC_UNAUTHORIZED,
+						"Vous n'avez pas les droits suffissants pour creer un utilisateur quelconques");
+			} else if (roleAbility.equals(Ability.Patron)) {
+				response.sendError(HttpServletResponse.SC_UNAUTHORIZED,
+						"Vous n'avez pas les droits requis pour creer un utilisateur ayant les droits "
+								+ roleAbility);
+
+			} else {
+				dao.persist(new User(username, DigestUtils.sha1Hex(password),
+						roleAbility));
+				response.setStatus(200);
+			}
+
+		} else {
+			response.sendError(HttpServletResponse.SC_BAD_REQUEST,
+					"La requete est mal formee");
 		}
 
 	}
-	
+
 	/**
 	 * to verify if a string is not null or empty
+	 * 
 	 * @param toTest
 	 * @return
 	 */
--- src/ihm/servletv2/RemoveUserServlet.java
+++ src/ihm/servletv2/RemoveUserServlet.java
@@ -1,5 +1,46 @@
 package ihm.servletv2;
 
-public class RemoveUserServlet {
+import java.io.IOException;
+
+import javax.inject.Inject;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import utilisateur.User;
+import dao.UserDAO;
+
+public class RemoveUserServlet extends HttpServlet {
+
+	@Inject
+	UserDAO userdao;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		String id = req.getParameter("id");
+		User user = (User) req.getSession().getAttribute("user");
+
+		if (id == null || id.isEmpty()) {
+			resp.sendError(HttpServletResponse.SC_BAD_REQUEST,
+					"La requete est mal formee");
+		} else {
+			User userToDelete = userdao.findById(new Long(id));
+			if (userToDelete == null) {
+				resp.sendError(HttpServletResponse.SC_BAD_REQUEST,
+						"L'utilisateur n'existe pas");
+			} else {
+				userdao.remove(userToDelete);
+
+			}
+		}
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		doGet(req, resp);
+	}
 
 }
