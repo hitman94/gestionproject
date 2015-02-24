df8e5189087cb58b9e5aa346ffc07d6ba67eae2d
jmeryam <jmeyam@gmail.com>
2015-02-24 18:25:36 +0100

    volume

--- WebContent/resources/js/volume.js
+++ WebContent/resources/js/volume.js
@@ -1,24 +1,27 @@
 /**
  * 
  */
-
+function supprimerVolume(id) {
+	
+	
+}
 $(document).ready(function() {
 	$('#errorsZone').hide();
 	
-	$("#submitCompany").click(function() {
+	$("#submitwp").click(function() {
 		var wp=document.getElementById("wp").value;
 		var volumeName = document.getElementById("volumeName").value;
 		
 		
 		$.ajax({
 	        type: 'POST',
-	        url: "createcompanyservlet",
+	        url: "CreateVolumeServlet",
 	        data: { title : volumeName,idWorkPackage:wp},
 	        success: function (data) {
 	        	window.location.replace("volume.jsp");
 	        },
 	        error: function (data , status, error){
-	        	$('#errorsZone').html("Erreur lors de la creation de l'entreprise.</br>" + error);
+	        	$('#errorsZone').html("Erreur lors de la creation du volume.</br>" + error);
 	        	$('#errorsZone').show(500);
 	        }
 	        
