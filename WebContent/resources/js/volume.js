8fc37c362a5e49e5f2f765fef40fd3f7c8cb4205
jmeryam <jmeyam@gmail.com>
2015-02-24 19:26:37 +0100

    m

--- WebContent/resources/js/volume.js
+++ WebContent/resources/js/volume.js
@@ -1,32 +1,46 @@
 /**
  * 
  */
-function supprimerVolume(id) {
-	
-	
+function supprimerVolume(idVolume) {
+	alert(idVolume);
+	$.ajax({
+		type: 'POST',
+		url: "DeleteVolumeServlet",
+		data: { idVolume : idVolume},
+		success: function (data) {
+			window.location.replace("volume.jsp");
+		},
+		error: function ( error){
+			$('#errorsZone').html("Erreur lors de la suppression de l'entreprise.</br>" + error);
+			$('#errorsZone').show(500);
+		}
+
+	});
+
+
 }
 $(document).ready(function() {
 	$('#errorsZone').hide();
-	
+
 	$("#submitwp").click(function() {
 		var wp=document.getElementById("wp").value;
 		var volumeName = document.getElementById("volumeName").value;
-		
-		
+
+
 		$.ajax({
-	        type: 'POST',
-	        url: "CreateVolumeServlet",
-	        data: { title : volumeName,idWorkPackage:wp},
-	        success: function (data) {
-	        	window.location.replace("volume.jsp");
-	        },
-	        error: function (data , status, error){
-	        	$('#errorsZone').html("Erreur lors de la creation du volume.</br>" + error);
-	        	$('#errorsZone').show(500);
-	        }
-	        
-	         });
-		
-		
+			type: 'POST',
+			url: "CreateVolumeServlet",
+			data: { title : volumeName,idWorkPackage:wp},
+			success: function (data) {
+				window.location.replace("volume.jsp");
+			},
+			error: function (data , status, error){
+				$('#errorsZone').html("Erreur lors de la creation du volume.</br>" + error);
+				$('#errorsZone').show(500);
+			}
+
+		});
+
+
 	});
 });
