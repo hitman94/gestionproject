2afa25daa625e3621d7f1e3b0e08096b39c4307f
Gazg <Flo.guif@gmail.com>
2015-02-24 17:09:31 +0100

    commit avant merge

--- src/dao/WorkPackageDAO.java
+++ src/dao/WorkPackageDAO.java
@@ -1,18 +1,14 @@
 package dao;
 
-import java.util.List;
-
 import javax.ejb.Stateless;
 import javax.inject.Named;
 
 import wpws.WorkPackage;
 
 
-
 @Stateless
 @Named
 public class WorkPackageDAO extends AbstractDAO<WorkPackage>{
-
 	public WorkPackageDAO() {
 		super(WorkPackage.class, "WorkPackage");
 	}
--- src/dao/WorkSpaceDAO.java
+++ src/dao/WorkSpaceDAO.java
@@ -1,7 +1,13 @@
 package dao;
 
+import javax.ejb.Stateless;
+import javax.inject.Named;
+
 import wpws.WorkSpace;
 
+
+@Stateless
+@Named
 public class WorkSpaceDAO extends AbstractDAO<WorkSpace> {
 	public WorkSpaceDAO() {
 		super(WorkSpace.class,"WorkSpace");
