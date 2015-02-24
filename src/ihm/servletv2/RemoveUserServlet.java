a6603ddfe61e796bf6d5040717177df144989402
ebabalac <ebabalac@gmail.com>
2015-02-24 14:50:08 +0100

    update RemoveUserServlet

--- src/ihm/servletv2/RemoveUserServlet.java
+++ src/ihm/servletv2/RemoveUserServlet.java
@@ -8,6 +8,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import comportement.Ability;
+
 import utilisateur.User;
 import dao.UserDAO;
 
@@ -20,21 +22,33 @@ public class RemoveUserServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 		String id = req.getParameter("id");
-		User user = (User) req.getSession().getAttribute("user");
-
+		User ownerRequest = (User) req.getSession().getAttribute("user");
 		if (id == null || id.isEmpty()) {
 			resp.sendError(HttpServletResponse.SC_BAD_REQUEST,
 					"La requete est mal formee");
+			return;
+		}
+		
+		User userTodelete = userdao.findById(new Long(id));
+		if (userTodelete == null) {
+			resp.sendError(HttpServletResponse.SC_BAD_REQUEST,
+					"L'utilisateur n'existe pas");
+			return;
+		} 
+		Ability userTodeleteAbility = userTodelete.getAbility();
+		Ability userAb = ownerRequest.getAbility();
+
+		// if a user try to delete other user or if the user to delete is the
+		// patron
+		if (userAb.equals(Ability.User)
+				|| userTodeleteAbility.equals(Ability.Patron)) {
+			resp.sendError(HttpServletResponse.SC_UNAUTHORIZED,
+					"Vous n'avez pas les droits requis pour cette opération");
 		} else {
-			User userToDelete = userdao.findById(new Long(id));
-			if (userToDelete == null) {
-				resp.sendError(HttpServletResponse.SC_BAD_REQUEST,
-						"L'utilisateur n'existe pas");
-			} else {
-				userdao.remove(userToDelete);
+					
+				userdao.remove(userTodelete);
 				resp.setStatus(200);
 
-			}
 		}
 	}
 
