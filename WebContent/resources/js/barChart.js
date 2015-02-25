13fb9588a21887774c4cb32f199d4fa49029f4de
Thomas Londechamp <totolond@hotmail.com>
2015-02-25 16:37:02 +0100

    update bar chart

--- WebContent/resources/js/barChart.js
+++ WebContent/resources/js/barChart.js
@@ -18,4 +18,20 @@ window.onload = function() {
 		            } 
 		            ]
 	}, {responsive : true});
+	
+	myBar.datasets[0].bars[0].fillColor = "rgba(216, 0, 0, 0.5)";
+	myBar.datasets[0].bars[0].strokeColor = "rgba(216, 0, 0, 0.8)";
+	myBar.datasets[0].bars[0].highlightFill = "rgba(216, 0, 0, 0.75)";
+	myBar.datasets[0].bars[0].highlightStroke = "rgba(216, 0, 0, 1)";
+	
+	myBar.datasets[0].bars[1].fillColor = "rgba(255, 165, 0, 0.5)";
+	myBar.datasets[0].bars[1].strokeColor = "rgba(255, 165, 0, 0.8)";
+	myBar.datasets[0].bars[1].highlightFill = "rgba(255, 165, 0, 0.75)";
+	myBar.datasets[0].bars[1].highlightStroke = "rgba(255, 165, 0, 1)";
+	
+	myBar.datasets[0].bars[3].fillColor = "rgba(50, 205, 50, 0.5)";
+	myBar.datasets[0].bars[3].strokeColor = "rgba(50, 205, 50, 0.8)";
+	myBar.datasets[0].bars[3].highlightFill = "rgba(50, 205, 50, 0.75)";
+	myBar.datasets[0].bars[3].highlightStroke = "rgba(50, 205, 50, 1)";
+    myBar.update();
 }
