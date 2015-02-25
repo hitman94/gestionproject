bb525b627226fd239437dbe97fdf142991f985ef
ebabalac <ebabalac@gmail.com>
2015-02-25 15:30:34 +0100

    update registerform on success

--- WebContent/resources/js/registerForm.js
+++ WebContent/resources/js/registerForm.js
@@ -18,44 +18,34 @@ function deleteUser(id) {
 	});
 }
 
-$('document').ready(
-		function() {
+$('document').ready(function() {
 
-			$('#username').on('click', function() {
-				$("#infoZone").css("display", "none");
-			});
-			$("#submit").click(
-					function() {
-						var name = $('#username').val();
-						var pass = $('#password').val();
+	$('#username').on('click', function() {
+		$("#infoZone").css("display", "none");
+	});
+	$("#submit").click(function() {
+		var name = $('#username').val();
+		var pass = $('#password').val();
 
-						$.ajax({
-							type : 'POST',
-							url : "CreateUserServlet",
-							data : {
-								username : name,
-								password : pass
-							},
-							success : function(data) {
+		$.ajax({
+			type : 'POST',
+			url : "CreateUserServlet",
+			data : {
+				username : name,
+				password : pass
+			},
+			success : function(data) {
 
-								$("#infoZone").addClass("alert alert-info");
+				window.location.replace("registerForm.jsp")
 
-								$("#infoZone").html(
-										"<p> L'utilisateur " + name
-												+ " a été crée</p>");
-								$("#infoZone").css("display", "");
-								setTimeout(function() {
-									window.location.replace("registerForm.jsp")
-								}, 3000);
-
-							},
-							error : function(data, status, error) {
-								$("#infoZone").addClass("alert alert-danger");
-								$("#infoZone").html("<p>" + error + " </p>");
-								$("#infoZone").css("display", "");
-								$("#infoZone").show(500);
-							}
-						});
-					});
-
+			},
+			error : function(data, status, error) {
+				$("#infoZone").addClass("alert alert-danger");
+				$("#infoZone").html("<p>" + error + " </p>");
+				$("#infoZone").css("display", "");
+				$("#infoZone").show(500);
+			}
 		});
+	});
+
+});
